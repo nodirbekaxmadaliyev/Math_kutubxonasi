15921a05af840d583c1279cbe61094a35e89e188
Nodirbek <Nodirbek@DESKTOP-SR49EP2>
2024-04-27 19:26:21 +0500

    Add project files.

---
+++ Math_kutubxonasi/Math_lib.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_kutubxonasi
+{
+    internal class Math_lib
+    {
+        public bool IsTub(long n)
+        {
+            for (int i = 2; i * i <= n; i++)
+                if (n % i == 0) return false;
+            return true;
+        }
+        public bool Fibonachi(long n)
+        {
+            if (n < 4) return true;
+            long a = 0, b = 1, c = a + b;
+            while (c < n)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            if (c == n) return true;
+            return false;
+        }
+        public void Fibonachi(long a, long b)
+        {
+            Console.WriteLine($"\t[{a}, {b}] oraliqdagi fibonachi sonlar : ");
+            if (a > b) (a, b) = (b, a);
+            long a1 = 0, a2 = 1, c = a1 + a2;
+            while(c < a)
+            {
+                c = a1 + a2;
+                a1 = a2;
+                a2 = c;
+            }
+            while(c <= b)
+            {
+                Console.Write(c + " ");
+                c = a1 + a2;
+                a1 = a2;
+                a2 = c;
+            }
+        }
+        public void Fibonachi(long a1, long a2, int n)
+        {
+            Console.WriteLine($"\tBerilgan boshlang'ich qiymatlar bilan {n} ta fibonachi son :");
+            if (n < 2) Console.WriteLine(a1);
+                else 
+            {
+                Console.Write($"{a1}, {a2} ");
+                long c;
+                while (n -- > 2)
+                {
+                    c = a1 + a2;
+                    Console.Write(c + " ");
+                    a1 = a2;
+                    a2 = c;
+                }
+            }
+            Console.WriteLine();
+        }
+        public double Pow(double a, int n)
+        {
+            if (n == 0) return 1;
+            double c = a;
+            while (n -- > 1)
+            {
+                c *= a;
+            }
+            return c;
+        }
+        public bool IsPerfect(long a)
+        {
+            if(a < 2) return false;
+            long sum = 1, i = 2;
+            for (; i * i < a; i ++)
+                if (a % i == 0)
+                    sum += i + (a / i);
+            if (i * i == a) sum += i;
+            if (sum == a) return true;
+            return false;            
+        }
+            
+    }
+}
---
+++ Math_kutubxonasi/Program.cs
@@ -0,0 +1,77 @@
+﻿using Math_kutubxonasi;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Math_lib obj = new Math_lib();
+        Console.WriteLine("Kichik dasturimizga xush kelibsiz!\n");
+        int n = 0;
+        do
+        {
+            Console.WriteLine("Kerakli bo'limni tanlang:\n");
+            Console.WriteLine(" 1 -> Tublikka tekshirish;\n\n 2 -> Fibonachi;\n\n 3 -> Daraja;\n\n 4 -> Mukammallikka tekshirish;\n\n 0 -> Tugatish;\n");
+            n = Convert.ToInt32(Console.ReadLine());
+            switch(n)
+            {
+                case 1:
+                    {
+                        Console.Clear();
+                        Console.Write("Kerakli sonni kiriting : "); ;
+                        long isTub = Convert.ToInt64(Console.ReadLine());
+                        Console.Write("\tKiritilgan son tub");
+                        if (!obj.IsTub(isTub)) Console.WriteLine(" emas\n");
+                        Console.ReadKey();
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\nKerakli bo'limni tanlang:\n\n 1 -> Kiritlgan son fibonachimi?\n\n 2 -> [a, b] oraliqdagi fibonachi sonlar;\n\n 3 -> a va b bilan boshlanuvchi N ta fibonachi sonlar;\n");
+                        int amal = Convert.ToInt32(Console.ReadLine());
+                        switch (amal)
+                        {
+                            case 1:
+                                Console.Write(" Tekshiriladigan sonni kiriting: ");
+                                long isFibonachi = Convert.ToInt64(Console.ReadLine());
+                                Console.Write(" Kiritilgan son fibonachi son");
+                                if (!obj.Fibonachi(isFibonachi)) Console.WriteLine(" emas");
+                                break;
+                            case 2:
+                                Console.WriteLine(" Kerakli oraliqni kiriting: ");
+                                string[] num = Console.ReadLine().Split();
+                                long x = Convert.ToInt64(num[0]), y = Convert.ToInt64(num[1]);
+                                Console.WriteLine($" [{x}, {y}] oraliqdagi fibonachi sonlar :\n");
+                                obj.Fibonachi(x, y);
+                                break;
+                            default:
+                                Console.WriteLine(" Boshlang'ich qiymatlar va fibonachi sonlar sonini kiriting:");
+                                string[] nums = Console.ReadLine().Split();
+                                long a1 = Convert.ToInt64(nums[0]), a2 = Convert.ToInt64(nums[1]);
+                                int count = Convert.ToInt32(nums[2]);
+                                Console.WriteLine($"{a1} va {a2} bilan boshlanuvchi {count} ta fibonachi son:");
+                                obj.Fibonachi(a1, a2, count);
+                                break;
+                        }
+                        Console.ReadKey();
+                        break;
+                    }
+                case 3: Console.WriteLine("Double: a va Int: n larni kiritng:");
+                        string[] s = Console.ReadLine().Split();
+                        double a = Convert.ToDouble(s[0]);
+                        int N = Convert.ToInt32(s[1]);
+                        Console.WriteLine($"\t{a} ^ {N} = {obj.Pow(a, N)}\n");
+                        Console.ReadKey();
+                        break;
+                case 4:
+                    Console.Write("\tTekshiriladigan sonni kiriting: ");
+                    long check = Convert.ToInt64(Console.ReadLine());
+                    Console.Write($" {check} soni mukammal son");
+                    if (!obj.IsPerfect(check)) Console.Write(" emas");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                    break;
+                 }
+            Console.Clear();
+        } while (n < 5 && n > 0);
+    }
+}
